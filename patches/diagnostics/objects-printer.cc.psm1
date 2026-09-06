cfacbe385ee3adeeb525d17aa657d1f0e1af0d9f
珊瑚海 <1624324687ym@gmail.com>
2026-09-06 21:16:13 +0800

    Fix Cast<> calls for V8 15.0 pointer-based cast API

--- patches/diagnostics/objects-printer.cc.psm1
+++ patches/diagnostics/objects-printer.cc.psm1
@@ -74,14 +74,14 @@ function Patch {
             -Patterns @('case FIXED_ARRAY_TYPE:', ';') `
             -Insert @"
       os << "\nStart FixedArray\n";
-      Cast<FixedArray>(*this)->FixedArrayPrint(os);
+      Cast<FixedArray>(this)->FixedArrayPrint(os);
       os << "\nEnd FixedArray\n";
 "@
         $Body = Add-LineBelow -Content $Body `
             -Patterns @('case OBJECT_BOILERPLATE_DESCRIPTION_TYPE:', ';') `
             -Insert @"
       os << "\nStart ObjectBoilerplateDescription\n";
-      Cast<ObjectBoilerplateDescription>(*this)
+      Cast<ObjectBoilerplateDescription>(this)
           ->ObjectBoilerplateDescriptionPrint(os);
       os << "\nEnd ObjectBoilerplateDescription\n";
 "@
@@ -89,7 +89,7 @@ function Patch {
             -Patterns @('case FIXED_DOUBLE_ARRAY_TYPE:', ';') `
             -Insert @"
       os << "\nStart FixedDoubleArray\n";
-      Cast<FixedDoubleArray>(*this)->FixedDoubleArrayPrint(os);
+      Cast<FixedDoubleArray>(this)->FixedDoubleArrayPrint(os);
       os << "\nEnd FixedDoubleArray\n";
 "@
         $Body = Add-LineBelow -Content $Body `
